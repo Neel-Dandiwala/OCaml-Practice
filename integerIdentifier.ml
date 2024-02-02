a8b0cd975185cfbb22b0dea23836519d81d6368a
Neel Dandiwala <neeldandi@gmail.com>
2024-02-02 18:57:05 -0500

    integer Identifier

---
+++ integerIdentifier.ml
@@ -0,0 +1,10 @@
+(*
+  This is a multi-line comment.
+  It can span multiple lines.
+*)
+
+let x_power_8 = 
+  let x_power_2 = x * x in 
+  let x_power_4 = x_power_2 * x_power_2 in 
+  let x_power_8 = x_power_4 * x_power_4 in 
+  x_power_8
